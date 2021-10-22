59a99a6bac7fc6a6138b9af78eee08a8a59d882d
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-10-23 01:26:41 +0200

    improve performance

--- src/components/incidenceView.vue
+++ src/components/incidenceView.vue
@@ -8,24 +8,24 @@
     </table><br />
     <!-- Datos del parte -->
     <table>
-        <tr v-if="incidence.owner.id != null">
+        <tr v-if="incidence.owner != null">
           <td>Nombre del empleado</td>
-          <td >{{incidence.owner.name}} {{incidence.owner.surname1}} {{incidence.owner.surname2}}</td>
+          <td >{{ incidence.owner }}</td>
         </tr>
         <tr>
           <td>Información</td>
-          <td v-if="incidence.state == 1 && user.permissions.includes('6') && incidence.owner.id == user.id && edit"><input type="text" name="issueDesc" v-model="issueDesc" required /></td>
+          <td v-if="incidence.state == 1 && user.permissions.includes('6') && edit"><input type="text" name="issueDesc" v-model="issueDesc" required /></td>
           <td v-else>{{ incidence.issueDesc }}</td>
         </tr>
-        <tr v-if="incidence.solver.id != null">
+        <tr v-if="incidence.solver != null">
           <td >Tecnico a cargo</td>
-          <td>{{ incidence.solver.name }} {{ incidence.solver.surname1 }} {{ incidence.solver.surname2 }}</td>
+          <td>{{ incidence.solver }}</td>
         </tr>
         <tr>
           <td>Fecha de creación</td>
           <td>{{incidence.initDateTime}}</td>
         </tr>
-        <tr v-if="incidence.state == 1 && user.permissions.includes('6') && incidence.owner.id == user.id">
+        <tr v-if="incidence.state == 1 && user.permissions.includes('6')">
           <td v-if="!edit" style="width:10%; height: 2%;">
             <a @click="deleteIncidence()" href="#">
               <img class="cierra" src="./delete.png" alt="Borrar incidencia" style="width:4%; height: 4%;"/>
@@ -37,12 +37,12 @@
             </a>
           </td>
         </tr>
-        <tr v-else-if="incidence.state == 1 && (user.permissions.includes('3') || user.permissions.includes('10')) && incidence.owner.id != user.id">
+        <tr v-else-if="incidence.state == 1 && (user.permissions.includes('3') || user.permissions.includes('10'))">
           <td colspan="2" v-if="!edit">
               <a href="#" @click="edit=true">Atender</a>
           </td>
         </tr>
-        <tr v-else-if="incidence.state == 2 && (user.permissions.includes('5') || user.permissions.includes('11')) && incidence.owner.id != user.id">
+        <tr v-else-if="incidence.state == 2 && (user.permissions.includes('5') || user.permissions.includes('11'))">
           <td colspan="2" v-if="!edit">
               <a href="#" @click="edit=true">Modificar</a>
           </td>
@@ -58,7 +58,7 @@
           </td>
         </tr>
     </table><br />
-    <div v-if="incidence.state == 1 && user.permissions.includes('6') && incidence.owner.id == user.id && edit">
+    <div v-if="incidence.state == 1 && user.permissions.includes('6')">
       <table v-if="edit">
         <tr>
           <th>Funciones</th>
@@ -73,7 +73,7 @@
       <div v-if="incidence.state != 1">
         <pieces-module :edit="edit" :pieces="incidence.pieces" @add="PieceIdsSelected.push($event)"/>
         <notes-module v-if="incidence.notes || edit" :edit="edit" :notes="incidence.notes" @add="note = $event"/>
-        <div v-else-if="incidence.state == 1 && (user.permissions.includes('3') || user.permissions.includes('10')) && incidence.owner.id != user.id">
+        <div v-else-if="incidence.state == 1 && (user.permissions.includes('3') || user.permissions.includes('10'))">
           <!-- attendIncidence -->
           <table v-if="edit">
             <tr>
@@ -88,7 +88,7 @@
             </tr>
           </table>
         </div>
-        <div v-else-if="incidence.state == 2 && (user.permissions.includes('5') || user.permissions.includes('11')) && incidence.owner.id != user.id">
+        <div v-else-if="incidence.state == 2 && (user.permissions.includes('5') || user.permissions.includes('11'))">
           <!-- modifyIncidence -->
           <table v-if="edit">
             <tr>
